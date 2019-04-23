db6d14fa35cec575af7af2fcb7cc8cb2771e8292
mindytai175 <mindytai175@gmail.com>
2019-04-23 12:27:45 +0800

    fix video auto play

--- src/components/Content/IndexPage/SwipeSlider.jsx
+++ src/components/Content/IndexPage/SwipeSlider.jsx
@@ -4,39 +4,12 @@ import PropTypes from 'prop-types'
 import video from '../../../video/promo2.mp4'
 
 class SwipeSlider extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      currentIndex: 0,
-    }
-    this.autoPlayInterval = null
-  }
-
-  componentDidMount(){
-    this.autoPlayInterval = setInterval(() => ( this.changeToNextSlider()), 5000)
-  }
-  
-  isActiveClass(i) {
-    return (this.state.currentIndex) === i ? "active" : ""
-  }
-
-  changeToNextSlider() {
-    const total = this.props.sliderDatas.length
-    let nextIndex = this.state.currentIndex + 1
-
-    if((nextIndex+1) > total) {nextIndex = 0}
-
-    this.setState({
-      currentIndex: nextIndex,
-    })
-  }
-
   render() {
     const { sliderDatas } = this.props
 
     return(
       <section className="swiper-slider">
-        <video autoPlay muted loop id="myVideo">
+        <video autoPlay muted playsInline loop id="myVideo">
 			    <source src={video}  type="video/mp4" />
 		    </video>
 
